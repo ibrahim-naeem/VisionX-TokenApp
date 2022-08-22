291c04c7275dd5d19231e2d25214be5818d24382
ibrahim-naeem <ibrahim22naeem@gmail.com>
2022-08-23 01:43:18 +0500

    New Feature Refactore

--- router.js
+++ router.js
@@ -3,19 +3,7 @@ const router = express.Router()
 const pool = require('./db')
 const jwt = require('jsonwebtoken')
 
-//Testing API
-router.post('/test', async(req, res)=>{
-    try {
-        //user_id generated automatically
-        const{user_id, name, age, token} = req.body;
 
-const data = await pool.query( "INSERT INTO user_details (name, age, token)VALUES ($1,$2,$3)", [name, age, token]);
-
-res.json({message: 'Data added to database successfully!!'});
-    } catch (error) {
-        console.error(error)
-    }
-})
 
 //Sign API
 router.post("/signToken", async(req, res)=>{
@@ -24,30 +12,60 @@ router.post("/signToken", async(req, res)=>{
     const token = jwt.sign({name, age},"112233",{expiresIn:'30d'})
 
     
-const data = await pool.query( "INSERT INTO user_details (name, age, token)VALUES ($1,$2,$3)", [name, age, token]);
+const data = await pool.query( "INSERT INTO user_info (token)VALUES ($1)", [token]);
 
-    res.status(201).json({TaskName: 'Sign', message: 'Data added to database successfully!!', name, age, token})
+    res.status(201).json({TaskName: 'Sign', message: 'Data added to database successfully!!', token})
     } catch (error) {
         console.error(error )
     }
 })
 
 //Get all users data
-router.get('/getAllUserDetails', async(req, res)=>{
+router.get('/users', async(req, res)=>{
     try {
-        const data = await pool.query("SELECT * FROM user_details");
+        const data = await pool.query("SELECT * FROM user_info");
         res.json(data.rows);
     } catch (error) {
         console.error(error)
     }
 })
 
-//Verify Token API
-router.post("/verifyToken", (req, res)=>{
-    const data = req.body.token;
-    const decode = jwt.verify(data, "112233")
-    res.status(202).json({TaskName : 'Verify', ...decode})
+//Get user full details
+router.get('/user/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const data = await pool.query("SELECT * FROM user_info WHERE user_id = $1", [id]);
+        const token = data.rows[0].token;
+        // res.json({token});
+        const decode = jwt.verify(token, "112233")
+        res.status(202).json({TaskName : 'Verify', ...decode})
+    } catch (error) {
+        console.error(error)
+    }
 })
 
+//Verify Token API
+// router.post("/verifyToken", (req, res)=>{
+//     const data = req.body.token;
+//     const decode = jwt.verify(data, "112233")
+//     res.status(202).json({TaskName : 'Verify', ...decode})
+// })
+
+
+
+
+// //Testing API
+// router.post('/test', async(req, res)=>{
+//     try {
+//         //user_id generated automatically
+//         const{user_id, name, age, token} = req.body;
+
+// const data = await pool.query( "INSERT INTO user_info (name, age, token)VALUES ($1,$2,$3)", [name, age, token]);
+
+// res.json({message: 'Data added to database successfully!!'});
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })
 module.exports = router;
 
